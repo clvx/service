180257058493d222d1028e2a68f9e3027335527f
clvx <michael.ibarra@gmail.com>
2022-05-14 18:25:19 -0500

    6 - middleware

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -25,13 +25,15 @@ type App struct {
 							// the ContextMux is plus shutdown. It's a way to 
 							// do a mux implementation without having to rewrite it
 	shutdown chan os.Signal
+	mw []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	return &App{
 		ContextMux: httptreemux.NewContextMux(),
 		shutdown: shutdown, // a way to initialize a clean shutdown from inside the application
+		mw: mw,
 	}
 }
 
@@ -43,15 +45,26 @@ func (a *App) SignalShutdown() {
 
 // Handle sets a handler function for a given HTTP method and a path pair
 // to the application server mux.
-func (a *App) Handle(method string, group string, path string, handler Handler) {
+func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
 
+	// First wrap handler specific middleware around this handler.
+	handler = wrapMiddleware(mw, handler)
+
+	// Add the application's general middleware to the handler chain
+	handler = wrapMiddleware(a.mw, handler)
+
+	// The function to execute for each request
 	h := func(w http.ResponseWriter, r *http.Request) {
-		// PRE CODE PROCESSING
+
+		// Inject Code
+
+		// Call the wrapped handler functions
 		if err := handler(r.Context(), w, r); err != nil {
-			// ERROR HANDLING
+
+			// INJECT CODE
 			return
 		}
-		// POST CODE PROCESSING
+		// INJECT CODE
 	}
 	finalPath := path
 	if group != "" {
